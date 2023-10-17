1362dc0e485efb43e67eb536dd5f7784529e6d50
Ivan Todorovic <ivan.t@coinis.com>
2023-10-17 18:09:05 +0200

    Updated packages to newer versions

--- src/main/java/com/example/demospringapp/domain/Car.java
+++ src/main/java/com/example/demospringapp/domain/Car.java
@@ -1,6 +1,6 @@
 package com.example.demospringapp.domain;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
--- src/main/java/com/example/demospringapp/domain/Model.java
+++ src/main/java/com/example/demospringapp/domain/Model.java
@@ -1,6 +1,6 @@
 package com.example.demospringapp.domain;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 @Entity
 public class Model {
     @Id
